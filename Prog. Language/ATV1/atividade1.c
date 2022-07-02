40fbc7b3cde0ba8a72451373a3245a06d813184c
paulovpp <paulovpp@gmail.com>
2022-07-02 20:01:23 -0300

    Pract log progr assignm and answers

---
+++ Prog. Language/ATV1/atividade1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int some_number;
+    printf("Hello World\n");
+    printf("Type something: ");
+    scanf("%d", &some_number);
+    printf("You typed: %d\n", some_number);
+}
---
+++ Prog. Language/ATV2/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Atividade 2 - Paulo Vinicius Pereira Pinheiro
+
+int main()
+{
+	int x, y, z;
+
+	// %p para ponteiros
+	printf("O endereço de x: %p \n", &x);
+	printf("O endereço de y: %p \n", &y);
+	printf("O endereço de z: %p \n", &z);
+	printf("O tamanho de x: %d \n", sizeof(x));
+}
